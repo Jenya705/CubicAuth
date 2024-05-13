82b9522e2790eedc175a90e902a8e7ad9ed87d37
Jenya705 <51133999+Jenya705@users.noreply.github.com>
2024-05-13 18:42:39 +0200

    Fixed NICKNAME_PATTERN

--- src/main/java/com/github/jenya705/cubicauth/handler/JoinHandler.java
+++ src/main/java/com/github/jenya705/cubicauth/handler/JoinHandler.java
@@ -30,7 +30,7 @@ import java.util.regex.Pattern;
 @RequiredArgsConstructor
 public class JoinHandler {
 
-    private static final Pattern NICKNAME_PATTERN = Pattern.compile("git ad");
+    private static final Pattern NICKNAME_PATTERN = Pattern.compile("[a-zA-Z0-9_]{3,16}");
 
     private final CubicAuth plugin;
 
